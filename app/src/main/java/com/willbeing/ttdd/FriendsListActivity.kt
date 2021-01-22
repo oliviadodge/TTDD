bc07a711cd706b224d14df4ebda1ffa1e7c72b22
Olivia <oliviadodge@gmail.com>
2021-01-22 11:13:11 -0800

    Add FriendsListActivity to AndroidManifest.xml and start on layout file

--- app/src/main/java/com/willbeing/ttdd/FriendsListActivity.kt
+++ app/src/main/java/com/willbeing/ttdd/FriendsListActivity.kt
@@ -1,7 +1,13 @@
 package com.willbeing.ttdd
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.fragment.app.FragmentActivity
 
 class FriendsListActivity: FragmentActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_friends_list)
+    }
 }
